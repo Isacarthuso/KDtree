9c908f6478046b9f2c8449076a686eccf7b146ee
Isac <isacarthuso@gmail.com>
2020-07-01 16:17:03 +0200

    KDTree KNN 3D

--- KDtree.cpp
+++ KDtree.cpp
@@ -1,18 +1,32 @@
 #include"KDtree.hpp"
 
 No* ROOT;
+//dimension = 0 -> 2D and dimension = 1 -> 3D
+bool dimension;
 
-
-void createKDtree(vector<vector<float>> points)
+void createKDtree(vector<vector<float>> points, bool dim)
 {
     vector<float> balancedPoint;
-    int dim;
-
-    dim = 0;
+    if(dim)
+     dimension = 1;
+    else
+     dimension = 0;
+    int i;
+    i = 0;
     do{
-        balancedPoint = choosetheponintToBalanceKDtree(points,0);
+        balancedPoint = choosetheponintToBalanceKDtree(points);
+        cout << "********************************* " << endl;
+        cout << "Size: " << points.size() << endl;
+        cout << "x: " << balancedPoint[0] << endl;
+        cout << "y: " << balancedPoint[1] << endl;
+        cout << "z: " << balancedPoint[2] << endl;
+        cout << "********************************* " << endl;
         insertPointAtTree(balancedPoint);
-        points.erase(points.begin() + balancedPoint[2]);
+        if(dimension)
+            points.erase(points.begin() + balancedPoint[3]);
+        else
+            points.erase(points.begin() + balancedPoint[2]);
+        i++;
     } while (!points.empty());
 }
 
@@ -29,6 +43,8 @@ void insertNo(No* previousNo, No* currentyNo, vector<float> point)
                 cout << "No Address: " << currentyNo << endl;
                 cout << "point x: " << currentyNo->noPoint[0] << endl;
                 cout << "point y: " << currentyNo->noPoint[1] << endl;
+                if (dimension)
+                    cout << "point z: " << currentyNo->noPoint[2] << endl;
                 cout << "Point Level: " << currentyNo->layer << endl;
                 cout << "Previous No: " << previousNo << endl;
                 cout << "Previous Rigth No: " << previousNo->nextNoRigth << endl;
@@ -52,6 +68,8 @@ void insertNo(No* previousNo, No* currentyNo, vector<float> point)
             cout << "No Address: " << currentyNo << endl;
             cout << "point x: " << currentyNo->noPoint[0] << endl;
             cout << "point y: " << currentyNo->noPoint[1] << endl;
+            if (dimension)
+                cout << "point z: " << currentyNo->noPoint[2] << endl;
             cout << "Point Level: " << currentyNo->layer << endl;
             cout << "Previous No: " << previousNo << endl;
             cout << "Previous Rigth No: " << previousNo->nextNoRigth << endl;
@@ -71,10 +89,15 @@ void insertNo(No* previousNo, No* currentyNo, vector<float> point)
         {
             previousNo->nextNoRigth = currentyNo;
             currentyNo->noPoint = point;
-            currentyNo->layer = 0;
+            if(dimension)
+                currentyNo->layer = 2;
+            else
+                currentyNo->layer = 0;
             cout << "No Address: " << currentyNo << endl;
             cout << "point x: " << currentyNo->noPoint[0] << endl;
             cout << "point y: " << currentyNo->noPoint[1] << endl;
+            if(dimension)
+                cout << "point z: " << currentyNo->noPoint[2] << endl;
             cout << "Point Level: " << currentyNo->layer << endl;
             cout << "Previous No: " << previousNo << endl;
             cout << "Previous Rigth No: " << previousNo->nextNoRigth << endl;
@@ -93,10 +116,15 @@ void insertNo(No* previousNo, No* currentyNo, vector<float> point)
         {
             previousNo->nextNoLeft = currentyNo;
             currentyNo->noPoint = point;
-            currentyNo->layer = 0;
+            if (dimension)
+                currentyNo->layer = 2;
+            else
+                currentyNo->layer = 0;
             cout << "No Address: " << currentyNo << endl;
             cout << "point x: " << currentyNo->noPoint[0] << endl;
             cout << "point y: " << currentyNo->noPoint[1] << endl;
+            if (dimension)
+                cout << "point z: " << currentyNo->noPoint[2] << endl;
             cout << "Point Level: " << currentyNo->layer << endl;
             cout << "Previous No: " << previousNo << endl;
             cout << "Previous Rigth No: " << previousNo->nextNoRigth << endl;
@@ -108,6 +136,56 @@ void insertNo(No* previousNo, No* currentyNo, vector<float> point)
             insertNo(previousNo->nextNoLeft, currentyNo, point);
         }
     }
+//For layer = 2
+    if (dimension)
+    {
+        if (previousNo->layer == 2 && point[2] >= previousNo->noPoint[2])
+        {
+            if (previousNo->nextNoRigth == NULL)
+            {
+                previousNo->nextNoRigth = currentyNo;
+                currentyNo->noPoint = point;
+               currentyNo->layer = 0;
+                cout << "No Address: " << currentyNo << endl;
+                cout << "point x: " << currentyNo->noPoint[0] << endl;
+                cout << "point y: " << currentyNo->noPoint[1] << endl;
+                cout << "point z: " << currentyNo->noPoint[2] << endl;
+                cout << "Point Level: " << currentyNo->layer << endl;
+                cout << "Previous No: " << previousNo << endl;
+                cout << "Previous Rigth No: " << previousNo->nextNoRigth << endl;
+                cout << "Previous Left No: " << previousNo->nextNoLeft << endl;
+                cout << "-----------------------------------------------------: " << endl;
+            }
+            else
+            {
+                insertNo(previousNo->nextNoRigth, currentyNo, point);
+            }
+        }
+
+        if (previousNo->layer == 2 && point[2] < previousNo->noPoint[2])
+        {
+            if (previousNo->nextNoLeft == NULL)
+            {
+                previousNo->nextNoLeft = currentyNo;
+                currentyNo->noPoint = point;
+                currentyNo->layer = 0;
+                cout << "No Address: " << currentyNo << endl;
+                cout << "point x: " << currentyNo->noPoint[0] << endl;
+                cout << "point y: " << currentyNo->noPoint[1] << endl;
+                cout << "point z: " << currentyNo->noPoint[2] << endl;
+                cout << "Point Level: " << currentyNo->layer << endl;
+                cout << "Previous No: " << previousNo << endl;
+                cout << "Previous Rigth No: " << previousNo->nextNoRigth << endl;
+                cout << "Previous Left No: " << previousNo->nextNoLeft << endl;
+                cout << "-----------------------------------------------------: " << endl;
+            }
+            else
+            {
+                insertNo(previousNo->nextNoLeft, currentyNo, point);
+            }
+        }
+    }
+
 }
 
 void insertPointAtTree(vector<float> point)
@@ -125,6 +203,8 @@ void insertPointAtTree(vector<float> point)
         cout << "ROOT ADDRESS: " << root << endl;
         cout << "point x: " << root->noPoint[0] << endl;
         cout << "point y: " << root->noPoint[1] << endl;
+         if( dimension )
+         cout << "point z: " << root->noPoint[2] << endl;
         cout << "Root Layer: " << root->layer << endl;
         cout << "-----------------------------------------------------: " << endl;
 
@@ -136,7 +216,7 @@ void insertPointAtTree(vector<float> point)
 
 }
 
-vector<float> choosetheponintToBalanceKDtree(vector<vector<float>> cloud, int region)
+vector<float> choosetheponintToBalanceKDtree(vector<vector<float>> cloud)
 {
     /*
     region = 0 -> x
@@ -144,7 +224,7 @@ vector<float> choosetheponintToBalanceKDtree(vector<vector<float>> cloud, int re
     region = 2 -> z
     */
     float large, menor, range, median, deltaP;
-    
+    int region = 0;
     vector<float> bestpoint;
 
     //initialization
@@ -163,14 +243,20 @@ vector<float> choosetheponintToBalanceKDtree(vector<vector<float>> cloud, int re
 
     //init
     deltaP = fabs(median - cloud[0][region]);
-    bestpoint = {{ cloud[0][0], cloud[0][1], 0 }};
+    if(dimension)
+        bestpoint = { { cloud[0][0], cloud[0][1],cloud[0][2], 0 } };
+    else
+        bestpoint = {{ cloud[0][0], cloud[0][1], 0 }};
     for (int index = 0; index < cloud.size(); index++)
     {
         
         if (fabs(median - cloud[index][region]) < deltaP)
         {
             deltaP = fabs(median - cloud[index][region]);
-            bestpoint = { { cloud[index][0],cloud[index][1], float(index) } };
+            if(dimension)
+                bestpoint = { { cloud[index][0],cloud[index][1], cloud[index][2],float(index) } };
+            else
+                bestpoint = { { cloud[index][0],cloud[index][1], float(index) } };
         }
     }
     return bestpoint;
@@ -188,23 +274,50 @@ vector<vector<float>> searchNearestNeighbors(vector<float> target, int tolerance
 void goThrougthTheTree(No* no, vector<float> target, int tolerance, vector<vector<float>>& NearestNeighbor)
 {
     vector<float> addNN;
+    float distance;
 
     if (no != NULL)
     {
         cout << "Search X: " << no->noPoint[0] << endl;
-        if ((no->noPoint[0] >= (target[0] - tolerance) && no->noPoint[0] <= (target[0] + tolerance)) &&
-            no->noPoint[1] >= (target[1] - tolerance) && no->noPoint[1] <= (target[1] + tolerance))
-        {
-            float distance = sqrt((no->noPoint[0] - target[0]) * (no->noPoint[0] - target[0]) +
-                (no->noPoint[1] - target[1]) * (no->noPoint[1] - target[1]));
-            std::cout << "Distance: " << distance << std::endl;
-            if (distance <= tolerance)
-            {
-                addNN.push_back(no->noPoint[0]);
-                addNN.push_back(no->noPoint[1]);
-                NearestNeighbor.push_back(addNN);
-            }
 
+        if (dimension)
+        {
+            if ((no->noPoint[0] >= (target[0] - tolerance) && no->noPoint[0] <= (target[0] + tolerance)) &&
+                (no->noPoint[1] >= (target[1] - tolerance) && no->noPoint[1] <= (target[1] + tolerance)) &&
+                (no->noPoint[2] >= (target[2] - tolerance) && no->noPoint[2] <= (target[2] + tolerance)))
+            {
+                distance = sqrt((no->noPoint[0] - target[0]) * (no->noPoint[0] - target[0]) +
+                                (no->noPoint[1] - target[1]) * (no->noPoint[1] - target[1]) +
+                                (no->noPoint[2] - target[2]) * (no->noPoint[2] - target[2]));
+
+                std::cout << "Distance: " << distance << std::endl;
+                if (distance <= tolerance)
+                {
+                    addNN.push_back(no->noPoint[0]);
+                    addNN.push_back(no->noPoint[1]);
+                    addNN.push_back(no->noPoint[2]);
+                    NearestNeighbor.push_back(addNN);
+                }
+
+            }
+        }
+
+        else
+        { 
+            if ((no->noPoint[0] >= (target[0] - tolerance) && no->noPoint[0] <= (target[0] + tolerance)) &&
+                no->noPoint[1] >= (target[1] - tolerance) && no->noPoint[1] <= (target[1] + tolerance))
+            {
+                distance = sqrt((no->noPoint[0] - target[0]) * (no->noPoint[0] - target[0]) +
+                    (no->noPoint[1] - target[1]) * (no->noPoint[1] - target[1]));
+                std::cout << "Distance: " << distance << std::endl;
+                if (distance <= tolerance)
+                {
+                    addNN.push_back(no->noPoint[0]);
+                    addNN.push_back(no->noPoint[1]);
+                    NearestNeighbor.push_back(addNN);
+                }
+
+            }
         }
 
         if (no->layer == 0)
@@ -223,6 +336,15 @@ void goThrougthTheTree(No* no, vector<float> target, int tolerance, vector<vecto
         
            if ((target[1] - tolerance) <= (no->noPoint[1]))
              goThrougthTheTree(no->nextNoLeft, target, tolerance, NearestNeighbor);
+        }
+
+        if (no->layer == 2)
+        {
+            if ((target[2] + tolerance) > (no->noPoint[2]))
+                goThrougthTheTree(no->nextNoRigth, target, tolerance, NearestNeighbor);
+
+            if ((target[2] - tolerance) <= (no->noPoint[2]))
+                goThrougthTheTree(no->nextNoLeft, target, tolerance, NearestNeighbor);
         }
     }
 }
